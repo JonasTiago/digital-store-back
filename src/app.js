ade596f04f31b1b6a38705b12049382a1c9738da
JonasTiago <jonastiago95@gmail.com>
2024-11-27 10:01:44 -0300

    feat: database config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import sequelize from "./config/db.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 // app.use("/users", usersRouter);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexão com o banco de dados bem-sucedida!");
+  })
+  .catch((err) => {
+    console.error("Não foi possível conectar ao banco de dados:", err);
+  });
 
 export default app;
---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+// src/config/database.js
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "postgres",
+    logging: false,
+  }
+);
+
+export default sequelize;
